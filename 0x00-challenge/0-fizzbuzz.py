ecb47aec7cf7caf9794b5afc68858d6aa45814be
Fares <123604805+FaresGomaa1@users.noreply.github.com>
2024-05-25 14:48:54 +0300

    Update 0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,35 +1,38 @@
+#!/usr/bin/python3
+""" FizzBuzz
+"""
 import sys
 
 
 def fizzbuzz(n):
-  """
-  FizzBuzz function prints numbers from 1 to n separated by a space.
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
 
-  - For multiples of three print "Fizz" instead of the number and for
-    multiples of five print "Buzz".
-  - For numbers which are multiples of BOTH three and five print "FizzBuzz".
-  """
-  if n < 1:
-    return
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
+    """
+    if n < 1:
+        return
 
-  tmp_result = []
-  for i in range(1, n + 1):
-    # Check for divisibility by 3 first, then 5
-    if i % 3 == 0:
-      tmp_result.append("Fizz")
-    elif i % 5 == 0:  # Only check for divisibility by 5 if not already Fizz
-      tmp_result.append("Buzz")
-    else:
-      tmp_result.append(str(i))
-  print(" ".join(tmp_result))
+    for i in range(1, n + 1):
+        # Check for divisibility by 3 and 5 first (combined condition)
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
 
 
 if __name__ == '__main__':
-  if len(sys.argv) <= 1:
-    print("Missing number")
-    print("Usage: ./0-fizzbuzz.py <number>")
-    print("Example: ./0-fizzbuzz.py 89")
-    sys.exit(1)
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
 
-  number = int(sys.argv[1])
-  fizzbuzz(number)
+    number = int(sys.argv[1])
+    fizzbuzz(number)
